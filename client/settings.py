34c4a9244bdf8278f1a25216df7f55a6a3aa81d7
Mason Lin <mason.lin1@hp.com>
2023-12-24 19:33:57 +0800

    Add offline game as base code

---
+++ client/settings.py
@@ -0,0 +1,15 @@
+# Game setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+
+# Player settings
+PLAYER_START_X = 400
+PLAYER_START_Y = 500
+PLAYER_SIZE = 0.35
+PLAYER_SPEED = 8
+
+# Bullet settings
+SHOOT_COOLDOWN = 5  # 20
+BULLET_SCALE = 1.4
+BULLET_SPEED = 20  # 10
